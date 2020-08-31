b170407d3efdb61915e4dd09c9b576a06cfb8c60
GVitSer <gvsbzn@mail.ru>
2020-08-31 12:00:21 +0300

    Add app umop5.2zEvenArray and task ru and en

---
+++ umop5.2zEvenArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5._2zEvenArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFirstEvenNumbers(10);
+            Console.ReadKey();
+        }
+        public static int[] GetFirstEvenNumbers(int count)
+        {
+            int[] array = new int[count];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 2 * (i + 1);
+                Console.WriteLine(array[i]);
+            }
+            return array;
+        }
+    }
+}
