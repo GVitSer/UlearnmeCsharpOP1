d9ebbfc74b5ab5c15f32bea5df381e1b51f20632
GVitSer <gvsbzn@mail.ru>
2020-08-31 11:52:00 +0300

    Add app umop5.3zMaximumIndex.

---
+++ umop5.3zMaximumIndex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5._3zMaximumIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] array= new[] { 1.0, 2.8, 3, 10, 18, 10, 2, 18 };
+            Console.WriteLine(Min(array));
+            Console.WriteLine(MaxIndex(array));
+            Console.ReadKey();
+        }
+        static double Min(double[] array)
+        {
+            var min = double.MaxValue;
+            foreach (var item in array)
+                if (item < min) min = item;
+            return min;
+        }
+        public static int MaxIndex(double[] array)
+        {
+            double max = 0;
+            int indexMax=-1;
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    indexMax = i;
+                }
+            }
+            return indexMax;
+        }
+    }
+}
