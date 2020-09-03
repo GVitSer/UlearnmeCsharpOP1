9c7c5c8e604676057fd9c84268788ac5502d4dac
GVitSer <gvsbzn@mail.ru>
2020-09-03 13:25:11 +0300

    Add app umop12.10zCreatingExtensionMethods

---
+++ umop12.10zCreatingExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop12o10zCreatingExtensionMethods
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var arg1 = "100500";
+            Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
+            Console.ReadKey();
+        }
+    }
+    public static class ToIntExtensions
+    {
+        public static int ToInt(this string arg)
+        {
+            return Convert.ToInt32(arg);
+        }
+    }
+
+}
