c02a2011ec8b290dd3c164c796ed7c073d260f40
GVitSer <gvsbzn@mail.ru>
2020-09-04 13:08:41 +0300

    Add app umop5.4zCount

---
+++ umop5.4zCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5o4zCount
+{
+    class Program
+    {
+        public static int GetElementCount(int[] items, int itemToCount)
+        {
+            int count = 0;
+            for (int i=0;i<items.Length;i++)
+            {
+                if (items[i] == itemToCount)
+                    count++;
+            }
+            return count;
+        }
+        static void Main()
+        {
+            int[] items = { 1, 2, 1, 1 };
+            Console.WriteLine(GetElementCount(items, 1));
+            Console.ReadKey();
+        }
+    }
+}
