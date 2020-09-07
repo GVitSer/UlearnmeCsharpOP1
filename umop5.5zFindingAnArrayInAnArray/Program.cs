cf7445a661ce605019377b94abbeb4d3816a455f
GVitSer <gvsbzn@mail.ru>
2020-09-07 17:02:28 +0300

    Add app umop5.5zFindingAnArrayInAnArray

---
+++ umop5.5zFindingAnArrayInAnArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5o5zFindingAnArrayInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 3, 4 };
+            int[] subArray = { 3, 4 };
+            Console.WriteLine(FindSubarrayStartIndex(array, subArray));
+            Console.ReadKey();
+        }
+        public static int FindSubarrayStartIndex(int[] array, int[] subArray)
+        {
+            for (var i = 0; i < array.Length - subArray.Length + 1; i++)
+                if (ContainsAtIndex(array, subArray, i))
+                    return i;
+            return -1;
+        }
+        public static bool ContainsAtIndex(int[] array, int[] subArray, int i)
+        {
+            bool real=true;
+            for (int j = 0; j < subArray.Length; j++)
+            {
+                if (array[j + i] == subArray[j])
+                    real = true;
+                else
+                {
+                    real = false; break;
+                }
+            }
+            return real;
+        }
+    }
+}
