7ccc3e74dec7d005bb732995e5f50e29e7da074d
GVitSer <69580250+GVitSer@users.noreply.github.com>
2020-09-14 21:48:18 +0300

    Add app umop5.7zTarotCards

---
+++ umop5.7zTarotCards/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5o7zTarotCards
+{
+    class Program
+    {
+		enum Suits
+		{
+			Wands,
+			Coins,
+			Cups,
+			Swords
+		}
+        private static string GetSuit(Suits suit)
+        {
+            return new string[] { "жезлов", "монет", "кубков", "мечей" }[(int)suit];
+        }
+
+        public static void Main()
+		{
+			Console.WriteLine(GetSuit(Suits.Wands));
+			Console.WriteLine(GetSuit(Suits.Coins));
+			Console.WriteLine(GetSuit(Suits.Cups));
+			Console.WriteLine(GetSuit(Suits.Swords));
+			Console.ReadKey();
+		}
+	}
+}
