613bc72d643e2a2679d7f67bdb6c332cd13b7506
GVitSer <gvs.bzn@mail.ru>
2020-09-14 23:22:09 +0300

    Add app umop4.6zFrame

---
+++ umop4.6zFrame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop4d6zFrame
+{
+    class Program
+    {
+		public static void Main()
+		{
+			WriteTextWithBorder("Menu:");
+			WriteTextWithBorder("");
+			WriteTextWithBorder(" ");
+			WriteTextWithBorder("Game Over!");
+			WriteTextWithBorder("Select level:");
+			Console.ReadKey();
+		}
+		private static void WriteTextWithBorder(string text)
+		{
+			Console.Write("+");
+			for (int i = 0; i < text.Length+2; i++)
+				Console.Write("-");
+			Console.WriteLine("+");
+			Console.Write("| ");
+			Console.Write(text);
+			Console.WriteLine(" |");
+			Console.Write("+");
+			for (int i = 0; i < text.Length + 2; i++)
+				Console.Write("-");
+			Console.WriteLine("+");
+		}
+	}
+}
