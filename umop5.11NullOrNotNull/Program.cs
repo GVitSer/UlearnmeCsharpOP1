c89eb4c248b9fe11b5feedbdc152afbf63860702
GVitSer <gvs.bzn@mail.ru>
2020-09-23 11:38:37 +0300

    Add app umop5.11NullOrNotNull

---
+++ umop5.11NullOrNotNull/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop5o11NullOrNotNull
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(CheckFirstElement(null));
+            Console.WriteLine(CheckFirstElement(new int[0]));
+            Console.WriteLine(CheckFirstElement(new[] { 1 }));
+            Console.WriteLine(CheckFirstElement(new[] { 0 }));
+            Console.ReadKey();
+        }
+        public static bool CheckFirstElement(int[] array)
+        {
+            return array != null && array.Length != 0 && array[0] == 0;
+        }
+    }
+}
