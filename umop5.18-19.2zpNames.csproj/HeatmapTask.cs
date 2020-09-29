c7a8bb720c4afdfb6b89ece9a2b1152ec2ea0079
GVitSer <gvs.bzn@mail.ru>
2020-09-29 12:21:24 +0300

    Change app umop5.18-19.2zpNames.csproj

---
+++ umop5.18-19.2zpNames.csproj/HeatmapTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static string[] NomerDayGet(NameData[] names)
+        {
+            var nomerDayArr = new string[30];
+            for (int i = 0; i < 30; i++)
+                nomerDayArr[i] = (i + 2).ToString();
+            return nomerDayArr;
+        }
+
+        public static string[] NomerMounthGet(NameData[] names)
+        {
+            var nomerMounthArr = new string[12];
+            for (int i = 0; i < 12; i++)
+            { nomerMounthArr[i] = (i + 1).ToString(); }
+            return nomerMounthArr;
+        }
+
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            string[] mounth = NomerMounthGet(names);
+            string[] day = NomerDayGet(names);
+            double[,] mounthDay = new double[30, 12];
+            foreach (var mname in names)
+                if (mname.BirthDate.Day != 1)
+                    mounthDay[mname.BirthDate.Day - 2, mname.BirthDate.Month - 1]++;
+            return new HeatmapData("Пример карты интенсивностей", mounthDay, day, mounth);
+        }
+    }
+}
