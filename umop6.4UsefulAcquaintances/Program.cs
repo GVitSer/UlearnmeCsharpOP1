9a1003ab23632076794ff3b3db1da974ab17fb54
GVitSer <gvs.bzn@mail.ru>
2020-10-02 20:47:06 +0300

    Add app umop6.4UsefulAcquaintances

---
+++ umop6.4UsefulAcquaintances/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop6o4UsefulAcquaintances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> contacts =new List <string> { "Ваня:ivan@grozniy.ru", "Ваня:v@mail.ru", 
+                "Ваня:vanechka@domain.com", "Вася:vasiliy@gmail.com", "Саша:a@lex.ru", "Саша:alex@nd.ru", 
+                "Саша:alexandr@yandex.ru", "Саша:sasha1995@sasha.ru", "Паша:p@p.ru", 
+                "Паша:pavel.egorov@urfu.ru"};
+            OptimizeContacts(contacts);
+            Console.ReadKey();
+        }
+        private static Dictionary<string, List<string>> OptimizeContacts(List<string> contacts)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+            foreach (var e in contacts)
+            {
+                var nameEmail = e.Split(':');
+                var key = e.Substring(0, Math.Min(2, nameEmail[0].Length));
+                if (!dictionary.ContainsKey(key))
+                {
+                    dictionary[key] = new List<string>();
+                }
+                dictionary[key].Add(e);
+            }
+            return dictionary;
+        }
+    }
+}
