a088fc4e00045411c89712c2c06c375d64dab799
GVitSer <gvs.bzn@mail.ru>
2020-10-06 18:35:28 +0300

    Add app umop6.8SplitAndJoin

---
+++ umop6.8SplitAndJoin/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace umop6o8SplitAndJoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string citiesPopulation = File.ReadAllText("states.txt");
+            Console.WriteLine(ReplaceIncorrectSeparators(citiesPopulation));
+            Console.ReadKey();
+        }
+        public static string ReplaceIncorrectSeparators(string text)
+        {
+            string[] separatorStrings = { " ", ":", ": ", ";", ",", "-", " - ", "\t" };
+            string[] words = text.Split(separatorStrings, System.StringSplitOptions.RemoveEmptyEntries);
+            string wordsTab = String.Join("\t", words);
+            return wordsTab;
+        }
+    }
+}
