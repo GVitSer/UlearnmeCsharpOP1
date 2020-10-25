53ce456320ff53031830ed4b51bb104803810105
GVitSer <gvs.bzn@mail.ru>
2020-10-26 00:28:29 +0300

    Add app umop6.19-21.2zpTextAnalysis.csproj

---
+++ umop6.19-21.2zpTextAnalysis.csproj/SentencesParserTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var listSentences = new List<List<string>>(); // Возвращаемый список
+            if (text == null) return null; //проверка на наличие текста
+            text = text.ToLower();
+            var sentences = text.Split(".:;?!()".ToCharArray(),//парсеный на предложения текст
+                StringSplitOptions.RemoveEmptyEntries);
+            foreach (var sent in sentences)
+            {
+                var listWords = new List<string>(); // Список для слов
+                var builder = new StringBuilder();
+                foreach (var ch in sent)
+                {
+                    if (char.IsLetter(ch) || ch == '\'')
+                        builder.Append(ch);
+                    else
+                        AddNotEmptyWord(builder, listWords);
+                }
+                AddNotEmptyWord(builder, listWords);
+                if (listWords.Count > 0)
+                    listSentences.Add(listWords);
+            }
+            return listSentences;
+        }
+
+        public static void AddNotEmptyWord(StringBuilder builder, List<string> listWords)
+        {
+            if (builder.Length > 0)
+            {
+                listWords.Add(builder.ToString());
+                builder.Clear();
+            }
+        }
+    }
+}
