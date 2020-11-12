363371f002debc8a8122dd245fef3043563a32f1
GVitSer <gvs.bzn@mail.ru>
2020-11-12 23:44:32 +0300

    Add app umop4.10-12.2zpMazes

---
+++ umop4.10-12.2zpMazes.csproj/DiagonalMazeTask.cs
@@ -0,0 +1,27 @@
+﻿namespace Mazes
+{
+    public static class DiagonalMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            int wh, hw;
+            wh = (width - 2) / (height - 2);
+            hw = (height - 2) / (width - 2);
+            while (robot.Finished == false)
+            {
+                if (width > height)
+                { Move(robot, wh, Direction.Right, Direction.Down); }
+                else
+                { Move(robot, hw, Direction.Down, Direction.Right); }
+            }
+        }
+        public static void Move(Robot robot, int aspectRatio, Direction direction, Direction directionTwo)
+        {
+            if (!robot.Finished) 
+                for (var i = 0; i < aspectRatio; i++) 
+                    robot.MoveTo(direction);
+            if (!robot.Finished) 
+                robot.MoveTo(directionTwo);
+        }
+    }
+}
---
+++ umop4.10-12.2zpMazes.csproj/SnakeMazeTask.cs
@@ -0,0 +1,33 @@
+﻿namespace Mazes
+{
+    public static class SnakeMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            int right = width - 3, down = 2;
+            while (robot.Finished == false)
+            {
+                MoveRight(robot, right);
+                MoveDown(robot, down);
+                MoveLeft(robot, right);
+                if (robot.Finished == false)
+                { MoveDown(robot, down); }
+            }
+        }
+        public static void MoveRight (Robot robot, int a)
+        {
+            for (int i = 0; i < a; i++)
+            { robot.MoveTo(Direction.Right); }
+        }
+        public static void MoveLeft(Robot robot, int a)
+        {
+            for (int i = 0; i < a; i++)
+            { robot.MoveTo(Direction.Left); }
+        }
+        public static void MoveDown (Robot robot, int a)
+        {
+            for (int i = 0; i < a; i++)
+            { robot.MoveTo(Direction.Down); }
+        }
+    }
+}
