e0a02674a1e5cade375a227a509bbd47fb298275
GVitSer <gvs.bzn@mail.ru>
2020-11-15 23:06:44 +0300

    Add app umop2.18.2zpBilliards

---
+++ umop2.18.2zpBilliards.csproj/BilliardsTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направелния движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            double a = (2 * wallInclinationRadians) - directionRadians;
+            return a;
+        }
+    }
+}
