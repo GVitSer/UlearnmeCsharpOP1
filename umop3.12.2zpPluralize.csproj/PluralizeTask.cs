5da1264b055a79bd0b1ba4bb244550c4a6baddec
GVitSer <gvs.bzn@mail.ru>
2020-11-16 21:41:21 +0300

    Add app umop3.12.2zpPluralize

---
+++ umop3.12.2zpPluralize.csproj/PluralizeTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			int r1 = count % 10;
+			int r2 = count % 100;
+			if ((r1 == 1) && (r2 != 11))
+				return "рубль";
+			else if ((r1 > 1) && (r1 < 5) && (r2 != 12) && (r2 != 13) && (r2 != 14))
+				return "рубля";
+			else return "рублей";
+		}
+	}
+}
