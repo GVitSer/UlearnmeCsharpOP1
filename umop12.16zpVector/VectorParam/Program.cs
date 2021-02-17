ff05a1473ad0e7e5d8a830a541fc8b5c5637fd26
GVitSer <gvs.bzn@mail.ru>
2021-02-17 03:58:18 +0300

    Add app umop12.16zpVector

---
+++ umop12.16zpVector/VectorParam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryTasks
+{
+    class Program
+    {
+        public static Vector
+            v1 = new Vector { X = 3.2, Y = 8.5 },
+            v2 = new Vector { X = 5.1, Y = 3.4 },
+            VSum = new Vector { X = 0, Y = 0 };
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Geometry.GetLength(v1));
+            Geometry.Add(v1, v2);
+            Console.WriteLine("Суммирующий вектор х={0} y={1}", Geometry.VSum.X, Geometry.VSum.Y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ umop12.16zpVector/VectorTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+    }
+
+    public static class Geometry
+    {
+        public static Vector
+            V1 = new Vector { X = 3.2, Y = 8.5 },
+            V2 = new Vector { X = 120.1, Y = 3.4 },
+            VSum = new Vector { X = 0, Y = 0 };
+        public static double DlinaV, S;
+        public static double GetLength(Vector v1)
+        {
+            S = v1.X * v1.X + v1.Y * v1.Y;
+            if (S > 0)
+            {
+                return DlinaV = Math.Sqrt(S);
+            }
+            else return 0;
+        }
+
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            VSum.X = v1.X + v2.X;
+            VSum.Y = v1.Y + v2.Y;
+            return VSum;
+        }
+    }
+}
