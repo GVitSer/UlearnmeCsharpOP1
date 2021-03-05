876a923f3b45c8f366c289bb3bf55ca2accc3da1
GVitSer <gvs.bzn@mail.ru>
2021-03-05 20:16:45 +0300

    Add app umop13.7zPrintEveryone

---
+++ umop13.7zPrintEveryone/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop13o7zPrintEveryone
+{
+    class Program
+    {
+        public static void Print(params object[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (i > 0)
+                    Console.Write(", ");
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Print(1, 2);
+            Print("a", 'b');
+            Print(1, "a");
+            Print(true, "a", 1);
+            Console.ReadKey();
+        }
+    }
+}
