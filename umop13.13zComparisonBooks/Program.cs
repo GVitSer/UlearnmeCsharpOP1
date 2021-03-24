f5300523c61a3b946bb2ccc0a1baea72cedc4287
GVitSer <gvs.bzn@mail.ru>
2021-03-24 22:05:43 +0300

    Add app umop13.13zComparisonBooks

---
+++ umop13.13zComparisonBooks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop13o13zComparisonBooks
+{
+    class Program
+    {
+        class Book : IComparable
+        {
+            public string Title;
+            public int Theme;
+
+            public int CompareTo(object obj)
+            {
+                var book = (Book)obj;
+                int themeCompare = Theme.CompareTo(book.Theme);
+                if (themeCompare != 0) return themeCompare;
+                return Title.CompareTo(book.Title);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var book1 = new Book { Title = "Axe", Theme = 4 };
+            var book2 = new Book { Title = "Age", Theme = 8 };
+            Console.WriteLine(book1.CompareTo(book2));
+            Console.ReadKey();
+        }
+    }
+}
