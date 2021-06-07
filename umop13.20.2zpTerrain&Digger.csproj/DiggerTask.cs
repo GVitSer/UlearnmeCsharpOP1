7e7d289b7c0cd74f908110ae27bb6152a4f8cc16
GVitSer <gvs.bzn@mail.ru>
2021-06-07 22:15:12 +0300

    Add app umop13.20.2zpTerrain&Digger.csproj

---
+++ umop13.20.2zpTerrain&Digger.csproj/DiggerTask.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digger
+{
+    //Напишите здесь классы Player, Terrain и другие.
+    public class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand { DeltaX = 0, DeltaY = 0 };
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 0;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
+    }
+
+    public class Player : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            Keys key = Game.KeyPressed;
+
+            switch (key)
+            {
+                case Keys.Down:
+                    if (y < Game.MapHeight - 1) return new CreatureCommand { DeltaX = 0, DeltaY = 1 };
+                    break;
+
+                case Keys.Up:
+                    if (y >= 1) return new CreatureCommand { DeltaX = 0, DeltaY = -1 };
+                    break;
+
+                case Keys.Right:
+                    if (x < Game.MapWidth - 1) return new CreatureCommand { DeltaX = 1, DeltaY = 0 };
+                    break;
+
+                case Keys.Left:
+                    if (x >= 1) return new CreatureCommand { DeltaX = -1, DeltaY = 0 };
+                    break;
+            }
+
+            return new CreatureCommand { DeltaX = 0, DeltaY = 0 };
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Digger.png";
+        }
+    }
+}
