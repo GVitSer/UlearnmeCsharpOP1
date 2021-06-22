f91e217d22f1c891f727eea6f20c36495c3951c0
GVitSer <gvs.bzn@mail.ru>
2021-06-22 17:21:53 +0300

    Add app umop14.10zVector

---
+++ umop14.10zVector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop14o10zVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check();
+            Console.ReadKey();
+        }
+		public static void Check()
+		{
+			Vector vector = new Vector(3, 4);
+			Console.WriteLine(vector.ToString());
+
+			vector.X = 0;
+			vector.Y = -1;
+			Console.WriteLine(vector.ToString());
+
+			vector = new Vector(9, 40);
+			Console.WriteLine(vector.ToString());
+
+			Console.WriteLine(new Vector(0, 0).ToString());
+		}
+        public class Vector
+        {
+            public double X;
+            public double Y;
+            public double Length
+            {
+                get { return Math.Sqrt(X * X + Y * Y); }
+            }
+
+            public Vector(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+            
+            public override string ToString()
+            {
+                return string.Format("({0}, {1}) with length: {2}", X, Y, Length);
+            }
+        }
+    }
+}
