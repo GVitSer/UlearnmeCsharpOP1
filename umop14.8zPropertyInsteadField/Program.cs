0f4c8ab7f809fa59fd549a71cb63e68e6ae779ca
GVitSer <gvs.bzn@mail.ru>
2021-06-22 14:07:51 +0300

    Add app umop14.8zPropertyInsteadField

---
+++ umop14.8zPropertyInsteadField/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umop14o8zPropertyInsteadField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check();
+            Console.ReadKey();
+        }
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+        public class Book
+        {
+            public string Title { get; set;}
+        }
+    }
+}
